d5f554ecef455091031613e295bdcc87a3f23781
buivantuan1998 <buivtuan103@gmail.com>
2024-08-22 00:48:53 +0700

    Add layout profile, add modal status

--- src/pages/charging-station/index.tsx
+++ src/pages/charging-station/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import { Box, Header, Page } from "zmp-ui";
+import React, { FC, useState } from "react";
+import { Box, Header, Page, Modal, Button } from "zmp-ui";
 import { Divider } from "components/divider";
 import { useNavigate } from "react-router";
 
@@ -9,13 +9,24 @@ import Help from "static/help.png";
 import Redirect2 from "static/redirect2.png";
 import PowerRed from "static/power-red.png";
 import PowerBule from "static/power-bule.png";
+import RenewableEnergy from "static/renewable-energy.png";
 
 const ChargingStationList: FC = () => {
   const navigate = useNavigate();
+  const [dialogVisible, setDialogVisible] = useState(false);
 
   const redirectDetailPage = () => {
     navigate('/charging-station/detail');
   }
+
+  const modalContent: FC = () => {
+    return (
+      <div className="text-[#fff]">
+        <div>aaaa</div>
+        <div>bbbb</div>
+      </div>
+    );
+  };
 
   return (
     <Box className="bg-background">
@@ -51,8 +62,18 @@ const ChargingStationList: FC = () => {
               <div><img className="w-5 h-5 mt-[4px]" src={PowerBule} /></div>
               <div className="flex">
                 <div className="text-[#42B96C] mr-[10px]">Cổng sạc sẵn sàng: 47/48</div>
-                <div className="bg-[#42B96C] w-[65px] h-[20px] text-[#fff] mr-[5px] rounded-lg text-center text-[12px]">Trạng thái</div>
-                <div className="bg-[#42B96C] w-[65px] h-[20px] text-[#fff] rounded-lg text-center text-[12px]" onClick={() => redirectDetailPage()}>Dịch vụ</div>
+                <div
+                  className="bg-[#42B96C] w-[65px] h-[20px] text-[#fff] mr-[5px] rounded-lg text-center text-[12px]"
+                  onClick={() => {setDialogVisible(true)}}
+                >
+                  Trạng thái
+                </div>
+                <div
+                  className="bg-[#42B96C] w-[65px] h-[20px] text-[#fff] rounded-lg text-center text-[12px]"
+                  onClick={() => redirectDetailPage()}
+                >
+                  Dịch vụ
+                </div>
               </div>
             </div>
           </div>
@@ -83,13 +104,41 @@ const ChargingStationList: FC = () => {
               <div><img className="w-5 h-5 mt-[4px]" src={PowerRed} /></div>
               <div className="flex">
                 <div className="text-[#f10101] mr-[10px]">Ngoài giờ làm việc</div>
-                <div className="bg-[#42B96C] w-[65px] h-[20px] text-[#fff] mr-[5px] rounded-lg text-center text-[12px]">Trạng thái</div>
-                <div className="bg-[#42B96C] w-[65px] h-[20px] text-[#fff] rounded-lg text-center text-[12px]" onClick={() => redirectDetailPage()}>Dịch vụ</div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      <Modal
+        visible={dialogVisible}
+        title="Trạng thái sạc"
+        onClose={() => {
+          setDialogVisible(false);
+        }}
+        verticalActions
+      >
+        <Box className="bg-[#232933]">
+          <div>
+            <div className="">
+              <div className="text-[#42B96C] mb-[10px] text-[16px] text-center">Trạng thái: Đang hoạt động</div>
+            </div>
+            <div className="text-[#42B96C] mb-[10px] text-[16px] text-center">Tiến trình sạc:</div>
+            <div className="flex justify-center">
+              <div><img className="w-9 h-9" src={RenewableEnergy} /></div>
+              <div className="grid items-center text-[#42B96C] ml-[15px] text-[20px]">35%</div>
+            </div>
+          </div>
+          <div className="grid justify-center">
+            <div
+             className="grid bg-[#006af5] w-[110px] h-[30px] text-center items-center rounded-xl font-bold mt-[20px]"
+             onClick={() => setDialogVisible(false)}
+            >
+              Đóng
+            </div>
+          </div>
+        </Box>
+      </Modal>
     </Box>
   );
 };
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,102 +1,88 @@
 import React, { FC } from "react";
-import { Box, Header, Icon, Page, Text } from "zmp-ui";
-import subscriptionDecor from "static/subscription-decor.svg";
-import { ListRenderer } from "components/list-renderer";
-import { useToBeImplemented } from "hooks";
+import { Box, Header, Page } from "zmp-ui";
 
-const Subscription: FC = () => {
-  const onClick = useToBeImplemented();
-  return (
-    <Box className="m-4" onClick={onClick}>
-      <Box
-        className="bg-green text-white rounded-xl p-4 space-y-2"
-        style={{
-          backgroundImage: `url(${subscriptionDecor})`,
-          backgroundPosition: "right 8px center",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <Text.Title className="font-bold">Đăng ký thành viên</Text.Title>
-        <Text size="xxSmall">Tích điểm đổi thưởng, mở rộng tiện ích</Text>
-      </Box>
-    </Box>
-  );
-};
+import User from "static/user.png";
+import Logout from "static/logout.png";
+import Discount from "static/discount.png";
+import RightArrow from "static/right-arrow.png";
+import CustomerService from "static/customer-service.png";
+import Question from "static/question.png";
+import Headphones from "static/headphones.png";
+import Policy from "static/policy.png";
 
 const Personal: FC = () => {
-  const onClick = useToBeImplemented();
+  const borderBottom = {
+    borderBottom: "1px solid #808080",
+  };
 
-  return (
-    <Box className="m-4">
-      <ListRenderer
-        title="Cá nhân"
-        onClick={onClick}
-        items={[
-          {
-            left: <Icon icon="zi-user" />,
-            right: (
-              <Box flex>
-                <Text.Header className="flex-1 items-center font-normal">
-                  Thông tin tài khoản
-                </Text.Header>
-                <Icon icon="zi-chevron-right" />
-              </Box>
-            ),
-          },
-          {
-            left: <Icon icon="zi-clock-2" />,
-            right: (
-              <Box flex>
-                <Text.Header className="flex-1 items-center font-normal">
-                  Lịch sử đơn hàng
-                </Text.Header>
-                <Icon icon="zi-chevron-right" />
-              </Box>
-            ),
-          },
-        ]}
-        renderLeft={(item) => item.left}
-        renderRight={(item) => item.right}
-      />
-    </Box>
-  );
-};
+  return(
+    <Box className="bg-background px-[10px]">
+      <div className="text-[#fff]">
+        <div className="bg-[#232933] grid mb-[20px] rounded-xl p-[10px] grid-2078">
+          <div className="grid items-center justify-center">
+            <img className="w-12 h-12" src={User} />
+          </div>
+          <div>
+            <div className="font-bold text-[16px]">Bùi Văn Tuấn</div>
+            <div className="text-[16px] text-gray">0332-0226-335</div>
+            <div className="text-[16px] text-gray">Hoạt động từ: 01/01/2024</div>
+          </div>
+        </div>
 
-const Other: FC = () => {
-  const onClick = useToBeImplemented();
+        <div className="bg-[#232933] grid mb-[20px] rounded-xl p-[15px]">
+          <div className="mb-[20px]">
+            <div className="grid grid-8836 mb-[20px]">
+              <div><img className="w-6 h-6" src={Discount} /></div>
+              <div>Khuyến mại</div>
+              <div><img className="w-5 h-5" src={RightArrow} /></div>
+            </div>
+            <div className="" style={borderBottom}></div>
+          </div>
 
-  return (
-    <Box className="m-4">
-      <ListRenderer
-        title="Khác"
-        onClick={onClick}
-        items={[
-          {
-            left: <Icon icon="zi-star" />,
-            right: (
-              <Box flex>
-                <Text.Header className="flex-1 items-center font-normal">
-                  Đánh giá đơn hàng
-                </Text.Header>
-                <Icon icon="zi-chevron-right" />
-              </Box>
-            ),
-          },
-          {
-            left: <Icon icon="zi-call" />,
-            right: (
-              <Box flex>
-                <Text.Header className="flex-1 items-center font-normal">
-                  Liên hệ và góp ý
-                </Text.Header>
-                <Icon icon="zi-chevron-right" />
-              </Box>
-            ),
-          },
-        ]}
-        renderLeft={(item) => item.left}
-        renderRight={(item) => item.right}
-      />
+          <div className="mb-[20px]">
+            <div className="grid grid-8836 mb-[20px]">
+              <div><img className="w-6 h-6" src={Question} /></div>
+              <div>Câu hỏi thường gặp</div>
+              <div><img className="w-5 h-5" src={RightArrow} /></div>
+            </div>
+            <div className="" style={borderBottom}></div>
+          </div>
+
+          <div className="mb-[20px]">
+            <div className="grid grid-8836 mb-[20px]">
+              <div><img className="w-6 h-6" src={CustomerService} /></div>
+              <div>Trung tâm trợ giúp</div>
+              <div><img className="w-5 h-5" src={RightArrow} /></div>
+            </div>
+            <div className="" style={borderBottom}></div>
+          </div>
+
+          <div className="mb-[20px]">
+            <div className="grid grid-8836 mb-[20px]">
+              <div><img className="w-6 h-6" src={Headphones} /></div>
+              <div>Yêu cầu hỗ trợ</div>
+              <div><img className="w-5 h-5" src={RightArrow} /></div>
+            </div>
+            <div className="" style={borderBottom}></div>
+          </div>
+
+          <div className="mb-[5px]">
+            <div className="grid grid-8836 mb-[20px]">
+              <div><img className="w-6 h-6" src={Policy} /></div>
+              <div>Điều khoản sử dụng</div>
+              <div><img className="w-5 h-5" src={RightArrow} /></div>
+            </div>
+            <div className="" style={borderBottom}></div>
+          </div>
+        </div>
+
+        <div className="bg-[#232933] grid mb-[10px] h-[60px] rounded-xl p-[10px] charging-history-grid-1285">
+          <div className="grid items-center justify-center">
+            <img className="w-5 h-5" src={Logout} />
+          </div>
+          <div className="grid items-center text-[16px] text-[#f64650]">Đăng xuất</div>
+        </div>
+      </div>
     </Box>
   );
 };
@@ -104,10 +90,8 @@ const Other: FC = () => {
 const ProfilePage: FC = () => {
   return (
     <Page>
-      <Header showBackIcon={false} title="&nbsp;" />
-      <Subscription />
+      <Header title="Tài khoản" showBackIcon={true} />
       <Personal />
-      <Other />
     </Page>
   );
 };
