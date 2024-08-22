cb9116eb81c7e0e89efa2c487ff7dc978883cab1
buivantuan1998 <buivtuan103@gmail.com>
2024-08-22 21:42:26 +0700

    update modal status, add modal service in screen charging battey

--- src/pages/charging-station/index.tsx
+++ src/pages/charging-station/index.tsx
@@ -10,23 +10,19 @@ import Redirect2 from "static/redirect2.png";
 import PowerRed from "static/power-red.png";
 import PowerBule from "static/power-bule.png";
 import RenewableEnergy from "static/renewable-energy.png";
+import ChargingStation from "static/charging-station-2.png";
+import Wifi from "static/wi-fi.png";
+import Restrooms from "static/restrooms.png";
+import Restaurant from "static/restaurant.png";
 
 const ChargingStationList: FC = () => {
   const navigate = useNavigate();
   const [dialogVisible, setDialogVisible] = useState(false);
+  const [dialogVisibleService, setDialogVisibleService] = useState(false);
 
   const redirectDetailPage = () => {
     navigate('/charging-station/detail');
   }
-
-  const modalContent: FC = () => {
-    return (
-      <div className="text-[#fff]">
-        <div>aaaa</div>
-        <div>bbbb</div>
-      </div>
-    );
-  };
 
   return (
     <Box className="bg-background">
@@ -70,7 +66,7 @@ const ChargingStationList: FC = () => {
                 </div>
                 <div
                   className="bg-[#42B96C] w-[65px] h-[20px] text-[#fff] rounded-lg text-center text-[12px]"
-                  onClick={() => redirectDetailPage()}
+                  onClick={() => setDialogVisibleService(true)}
                 >
                   Dịch vụ
                 </div>
@@ -112,7 +108,7 @@ const ChargingStationList: FC = () => {
 
       <Modal
         visible={dialogVisible}
-        title="Trạng thái sạc"
+        title="Đang hoạt động"
         onClose={() => {
           setDialogVisible(false);
         }}
@@ -120,19 +116,69 @@ const ChargingStationList: FC = () => {
       >
         <Box className="bg-[#232933]">
           <div>
-            <div className="">
-              <div className="text-[#42B96C] mb-[10px] text-[16px] text-center">Trạng thái: Đang hoạt động</div>
-            </div>
-            <div className="text-[#42B96C] mb-[10px] text-[16px] text-center">Tiến trình sạc:</div>
-            <div className="flex justify-center">
+            {/* <div className="">
+              <div className="mb-[15px] text-[24px] text-center">Đang hoạt động</div>
+            </div> */}
+            <div className="flex justify-center mb-[15px]">
               <div><img className="w-9 h-9" src={RenewableEnergy} /></div>
-              <div className="grid items-center text-[#42B96C] ml-[15px] text-[20px]">35%</div>
+              <div className="grid items-center text-[#42B96C] ml-[15px] text-[20px]">36%</div>
+            </div>
+            <div className="flex justify-center mb-[15px]">
+              <div><img className="w-9 h-9" src={RenewableEnergy} /></div>
+              <div className="grid items-center text-[#42B96C] ml-[15px] text-[20px]">62%</div>
+            </div>
+            <div className="flex justify-center mb-[15px]">
+              <div><img className="w-9 h-9" src={RenewableEnergy} /></div>
+              <div className="grid items-center text-[#42B96C] ml-[15px] text-[20px]">45%</div>
             </div>
           </div>
           <div className="grid justify-center">
             <div
              className="grid bg-[#006af5] w-[110px] h-[30px] text-center items-center rounded-xl font-bold mt-[20px]"
              onClick={() => setDialogVisible(false)}
+            >
+              Đóng
+            </div>
+          </div>
+        </Box>
+      </Modal>
+
+      <Modal
+        visible={dialogVisibleService}
+        title="Dịch vụ"
+        onClose={() => {
+          setDialogVisibleService(false);
+        }}
+        verticalActions
+      >
+        <Box className="bg-[#232933]">
+          <div className="px-[15px] mt-[15px]">
+            <div className="grid charging-history-grid-1285 my-[5px]">
+              <div><img className="w-7 h-7 mt-[4px]" src={ChargingStation} /></div>
+              <div className="ml-[10px] text-[16px] grid items-center">Số trụ đang trống là 2</div>
+            </div>
+
+            <div className="grid charging-history-grid-1285 my-[5px]">
+              <div><img className="w-7 h-7 mt-[4px]" src={Restrooms} /></div>
+              <div className="ml-[10px] text-[16px] grid items-center">Có nhà vệ sinh</div>
+            </div>
+
+            <div className="grid charging-history-grid-1285 my-[5px]">
+              <div><img className="w-7 h-7 mt-[4px]" src={Restaurant} /></div>
+              <div className="ml-[10px] text-[16px] grid items-center">Có bán đồ ăn uống</div>
+            </div>
+
+            <div className="grid charging-history-grid-1285 my-[5px]">
+              <div><img className="w-7 h-7 mt-[4px]" src={Wifi} /></div>
+              <div className="ml-[10px] text-[16px] grid items-center">Có wifi</div>
+            </div>
+
+            <div className="h-[15px]"></div>
+          </div>
+          <div className="grid justify-center">
+            <div
+             className="grid bg-[#006af5] w-[110px] h-[30px] text-center items-center rounded-xl font-bold mt-[20px]"
+             onClick={() => setDialogVisibleService(false)}
             >
               Đóng
             </div>
